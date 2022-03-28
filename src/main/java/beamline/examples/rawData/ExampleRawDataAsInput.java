80a891bfb62aedd031af28a0107119f0137d2f42
Andrea Burattin <andrea.burattin@gmail.com>
2022-03-28 16:18:06 +0200

    Fixed example

--- src/main/java/beamline/examples/rawData/ExampleRawDataAsInput.java
+++ src/main/java/beamline/examples/rawData/ExampleRawDataAsInput.java
@@ -1,13 +1,17 @@
 package beamline.examples.rawData;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 
 import beamline.events.BEvent;
 import beamline.exceptions.EventException;
+import beamline.miners.trivial.DirectlyFollowsDependencyDiscoveryMiner;
+import beamline.miners.trivial.ProcessMap;
 import beamline.sources.BeamlineAbstractSource;
 
 public class ExampleRawDataAsInput {
@@ -29,7 +33,7 @@ public class ExampleRawDataAsInput {
 					String activityName = line.substring(3);
 					
 					try {
-						ctx.collect(BEvent.create("my-process-name", activityName, caseId));
+						ctx.collect(BEvent.create("my-process-name", caseId, activityName));
 					} catch (EventException e) {
 						e.printStackTrace();
 					}
@@ -41,8 +45,15 @@ public class ExampleRawDataAsInput {
 		env
 			.addSource(customSource)
 			.keyBy(BEvent::getProcessName)
-//			.keyBy(BEvent::getTraceName)
-			.print();
+			.flatMap(new DirectlyFollowsDependencyDiscoveryMiner().setModelRefreshRate(1).setMinDependency(0.1))
+			.addSink(new SinkFunction<ProcessMap>() {
+				private static final long serialVersionUID = 5088089920578609906L;
+
+				@Override
+				public void invoke(ProcessMap value, Context context) throws Exception {
+					value.generateDot().exportToSvg(new File("src/main/resources/output/output.svg"));
+				};
+			});
 		env.execute();
 	}
 }
