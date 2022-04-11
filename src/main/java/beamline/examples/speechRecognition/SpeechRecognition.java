a81dc4fbc9cb1d575c6c536bea94f5094ef7d4b7
Andrea Burattin <andrea.burattin@gmail.com>
2022-04-11 15:55:10 +0200

    Removed old code

--- src/main/java/beamline/examples/speechRecognition/SpeechRecognition.java
+++ src/main/java/beamline/examples/speechRecognition/SpeechRecognition.java
@@ -12,25 +12,6 @@ import beamline.miners.trivial.ProcessMap;
 public class SpeechRecognition {
 
 	public static void main(String...args) throws Exception {
-//		System.out.println("starting...");
-//		TrivialDiscoveryMiner miner = new TrivialDiscoveryMiner();
-//		miner.setModelRefreshRate(1);
-//		miner.setMinDependency(0);
-//
-//		// in the following statement we set a hook to save the map every 1000 events processed
-//		miner.setOnAfterEvent(() -> {
-//			if (miner.getProcessedEvents() % 2 == 0) {
-//				try {
-//					File f = new File("src/main/resources/output/output.svg");
-//					miner.getLatestResponse().generateDot().exportToSvg(f);
-//				} catch (IOException e) { }
-//			}
-//		});
-//		
-//		// connects the miner to the actual source
-//		XesSource source = new SpeechRecognizerSource();
-//		source.prepare();
-//		source.getObservable().subscribe(miner);
 		System.out.println("starting...");
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		env
